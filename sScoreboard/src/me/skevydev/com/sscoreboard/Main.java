077f50a132a42f0aa4217f249ef04b91ca69a2e6
SkevyDev <35754426+SkevyDev@users.noreply.github.com>
2018-05-25 10:24:10 -0300

    Atualizando
    sScoreboard

--- sScoreboard/src/me/skevydev/com/sscoreboard/Main.java
+++ sScoreboard/src/me/skevydev/com/sscoreboard/Main.java
@@ -8,6 +8,10 @@ import me.skevydev.com.sscoreboard.events.JoinScore;
 
 public class Main extends JavaPlugin {
 
+	private static Economy economy;
+	public static Economy getEconomy(){
+		return economy;
+	}
 	public void onEnable() {
 		Bukkit.getConsoleSender().sendMessage("§aPlugin §fsScoreboard §ahabilitado com sucesso!");
 		setupCommands();
@@ -24,6 +28,14 @@ public class Main extends JavaPlugin {
 	
 	void setupEvents() {
 		Bukkit.getPluginManager().registerEvents(new JoinScore(), this);
+	}
+	
+	private boolean setupEconomy() {
+		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(Economy.class);
+		if(economyProvider != null) {
+			economy = economyProvider.getProvider();
+		}
+		return (economy != null);
 	}
 	
 }
